a6e0abc7268e8ae079a5bdff6f3bfbd247deb6d1
zbhxlj <2452078981@qq.com>
2021-03-10 10:10:40 +0800

    initial commit

--- apps/webget.cc
+++ apps/webget.cc
@@ -3,6 +3,7 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -16,9 +17,25 @@ void get_URL(const string &host, const string &path) {
     // Then you'll need to print out everything the server sends back,
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
+    stringstream ss;
+    ss << "GET " << path << " HTTP/1.1" << "\r\n";
+    ss << "Host: " << host << "\r\n";
+    ss << "Connection:   close" << "\r\n";
+    ss << "\r\n";
+    
+    // cout << ss.str();
+    Address addr(host, "http");
+    
+    TCPSocket httpSocket;
+    httpSocket.connect(addr);
+    httpSocket.write(ss.str());
 
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    while(!httpSocket.eof()){
+        cout << httpSocket.read();
+    }
+
+    // cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    // cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main(int argc, char *argv[]) {
