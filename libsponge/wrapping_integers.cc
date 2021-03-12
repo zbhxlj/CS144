2a96c9f0609fb47d9a2d816eed3b13dec6d14f5b
zbhxlj <2452078981@qq.com>
2021-03-12 21:38:41 +0800

    finished integer_wrap

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,5 +1,8 @@
 #include "wrapping_integers.hh"
-
+// #define LOG
+#ifdef LOG
+#include<dbg.h>
+#endif
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -14,8 +17,11 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    const uint64_t mod = 1ULL << 32;
+
+    return WrappingInt32{
+        static_cast<uint32_t>((static_cast<uint32_t>(n) + isn.raw_value()) % mod)
+    };
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +35,30 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    const uint64_t mod = 1ULL << 32;
+    uint64_t low = ((n.raw_value() + mod - isn.raw_value()) % mod);
+    //! checkpoint 为0 时，不可能向前减去 mod， 所以范围是[0, 2^32 - 1]， 正好就是low的范围
+    if(checkpoint == 0) return low;
+    
+    uint64_t mid = checkpoint & (static_cast<uint64_t>(~(mod - 1)));
+    // ! 注意这里的取值范围 [ )
+    const __int128_t mod128 = static_cast<__int128_t>(1) << 64;
+    uint64_t offset = static_cast<uint64_t>((static_cast<__uint128_t>(mid) + static_cast<__uint128_t>(low) + mod128 - static_cast<__uint128_t>(checkpoint)) % mod128);
+    
+    if(offset < static_cast<uint64_t>(1ULL << 31) || offset >= static_cast<uint64_t>(mod128 - static_cast<__uint128_t>(1ULL << 31))){
+        #ifdef LOG
+            dbg(mid + low);
+        #endif
+        return mid + low;
+    }else if(offset > static_cast<uint64_t>(1ULL << 31) && offset < static_cast<uint64_t>(1ULL << 48)){
+        #ifdef LOG
+            dbg(mid + low - mod);
+        #endif
+        return mid + low - mod;
+    }else{
+        #ifdef LOG
+            dbg(mid + low + mod);
+        #endif
+        return mid + low + mod;
+    }
 }
